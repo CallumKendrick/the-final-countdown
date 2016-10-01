41276efdfce1dcfa4c070765fc75c31487bd27af
CallumKendrick <callumkendrick@hotmail.co.uk>
2016-10-01 15:44:00 +0100

    First valid build for watchface from tutorial

--- watchface/src/c/main.c
+++ watchface/src/c/main.c
@@ -4,24 +4,24 @@ static Window *s_main_window;
 
 static void main_window_load(Window *window)
 {
-  
+
 }
 
 static void main_window_unload(Window *window)
 {
-  
+
 }
 
 static void init()
 {
     s_main_window = window_create();
-  
-    window_set_window_handlers(s_mainWindow, (WindowHandlers)
+
+    window_set_window_handlers(s_main_window, (WindowHandlers)
                                {
-                                   .load = main_window_load;
-                                   .unload = main_window_unload;
+                                   .load = main_window_load,
+                                   .unload = main_window_unload
                                });
-  
+
     //Push to main window with animated true
     window_stack_push(s_main_window, true);
 }
